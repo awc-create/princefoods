2a78c26fabc787a23ae2cfd65a948dcdf8738f09
Adnan 2025 <adaptiveworkflowconsultancy@gmail.com>
2025-09-10 23:05:45 +0100

    ci: trigger pipeline

--- next.config.js
+++ next.config.js
@@ -1,27 +1,30 @@
+// site/next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone', // <- required to emit .next/standalone
   reactStrictMode: true,
   trailingSlash: true,
+  eslint: { ignoreDuringBuilds: true },
+
   images: {
+    unoptimized: true, // works well behind a reverse-proxy/CDN
     remotePatterns: [
-      // Wix
+      { protocol: 'https', hostname: 'utfs.io', pathname: '/**' },
       { protocol: 'https', hostname: 'static.wixstatic.com', pathname: '/**' },
       { protocol: 'https', hostname: 'images.wixstatic.com', pathname: '/**' },
-
-      // Google Drive direct view endpoints (if you actually serve images from there)
       { protocol: 'https', hostname: 'drive.google.com', pathname: '/**' },
-
-      // Google Photos / user content CDNs
       { protocol: 'https', hostname: 'lh3.googleusercontent.com', pathname: '/**' },
       { protocol: 'https', hostname: 'lh4.googleusercontent.com', pathname: '/**' },
       { protocol: 'https', hostname: 'lh5.googleusercontent.com', pathname: '/**' },
       { protocol: 'https', hostname: 'lh6.googleusercontent.com', pathname: '/**' },
-      { protocol: 'https', hostname: 'ssl.gstatic.com', pathname: '/**' },
-
-      // Add any other CDNs you use explicitly
-      // { protocol: 'https', hostname: 'cdn.example.com', pathname: '/**' },
-    ],
+      { protocol: 'https', hostname: 'ssl.gstatic.com', pathname: '/**' }
+    ]
   },
+
+  // make sure native/optional deps get traced into the standalone build
+  outputFileTracingIncludes: {
+    '/**/*': ['./node_modules/bcryptjs/**']
+  }
 };
 
 module.exports = nextConfig;
